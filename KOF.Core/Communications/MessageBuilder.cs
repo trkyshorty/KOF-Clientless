d2fbc01b91b314b643df0c2a603558d090221a63
Türkay Tanrıkulu <trky.shorty@gmail.com>
2023-01-06 22:42:59 +0300

    Update crc

--- KOF.Core/Communications/MessageBuilder.cs
+++ KOF.Core/Communications/MessageBuilder.cs
@@ -381,7 +381,7 @@ public class MessageBuilder
 
         msg.Write(commandType);
         msg.Write<byte>(Convert.FromHexString(crc));
-        msg.Write(3);
+        msg.Write(2);
 
         return msg;
     }
--- KOF.Core/Services/LoginService.cs
+++ KOF.Core/Services/LoginService.cs
@@ -99,7 +99,7 @@ public partial class LoginService
             case 0:
             case 1:
             case 2:
-                return session.SendAsync(MessageBuilder.MsgSend_HackTool(6, "91e56f24"));
+                return session.SendAsync(MessageBuilder.MsgSend_HackTool(6, "b32c6240"));
 
             case 17: // not signed in -> last login check.
                 return session.DisconnectAsync();
