6fd262925ed208e8bf12bff7f8f2d4a7a0f9ad6a
newQne <109394356+newQne@users.noreply.github.com>
2023-08-06 08:38:45 +0300

    CRC Changes

--- KOF.Core/Services/LoginService.cs
+++ KOF.Core/Services/LoginService.cs
@@ -99,7 +99,7 @@ public partial class LoginService
             case 0:
             case 1:
             case 2:
-                return session.SendAsync(MessageBuilder.MsgSend_HackTool(6, "70e725bf"));
+                return session.SendAsync(MessageBuilder.MsgSend_HackTool(6, "cfe6024c"));
 
             case 17: // not signed in -> last login check.
                 return session.DisconnectAsync();
